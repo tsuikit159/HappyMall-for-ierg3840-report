f72bc449dfb5b05bb2885b54586deecfc235187a
tsuikit159 <tsuikit159@gmail.com>
2023-04-17 02:29:29 +0800

    uninstall useless stuff

--- project/pages/_app.js
+++ project/pages/_app.js
@@ -1,6 +1,4 @@
 import '@/styles/globals.css'
-import { useEffect } from "react";
-import { CartProvider } from 'use-shopping-cart';
 import { RecoilRoot } from "recoil"
 import { Toaster } from 'react-hot-toast'
 export default function App({ Component, pageProps }) {
